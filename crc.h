3faead11a693a26f3871c0749370f9c911fbefe0
Victor Lomax <victorlomax@users.noreply.github.com>
2015-08-13 22:21:52 -0400

    Create crc.h

---
+++ crc.h
@@ -0,0 +1,21 @@
+// ----------------------------------------------------------------------------
+//	CCITT-CRC16 calculator
+// ----------------------------------------------------------------------------
+class CRC {
+  public:
+    CRC() { result[0]=result[1]=0xff; }
+    void compute(uchar byte) 
+    { 
+	    uchar t = 8; 
+	    result = result ^ byte << 8; 
+      do 
+	    { 
+		    if (result & 0x8000) 
+			    result = result << 1 ^ 0x1021; 
+		    else 
+			    result = result << 1; 
+	    } while (--t); 
+	  }
+	  private:
+	    uchar result[2];
+}
