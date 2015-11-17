d4bca6b6eb0cc70ef43d01b1e90d26625e729073
Victor Lomax <victorlomax@users.noreply.github.com>
2015-11-17 13:55:29 -0500

    Update mb8877.h

--- mb8877.h
+++ mb8877.h
@@ -5,27 +5,17 @@
 
 -- History
 2015 Jan 28: v2.0	Restart working after crash
+2015 Nov 18: v2.1	Split this file into several
 
 -- References
 * Arduino Mini: http://arduino.cc/en/Main/ArduinoBoardMini
 * SD library: http://www.roland-riegel.de/sd-reader/index.html
 * MicroFAT: http://arduinonut.blogspot.ca/2008/04/ufat.html
-* mb8877a: from RetroPC ver 2006.12.06 by Takeda.Toshiya, http://homepage3.nifty.com/takeda-toshiya/‎
+* mb8877a: from RetroPC ver 2006.12.06 by Takeda.Toshiya, http://homepage3.nifty.com/takeda-toshiya/?
 ========================================== */
 
 #ifndef _H_MB8877
 #define _H_MB8877
-
-#define FDC_DEBUG
-#define drvreg 1
-
-#ifndef uchar
-#define unsigned char uchar
-#endif
-
-#ifndef uint
-#define unsigned int uint
-#endif
 
 // MB8877 variables
 #define FDC_ST_BUSY		0x01	// busy
@@ -81,62 +71,6 @@
 #define FDC_SEEK_FORWARD	true
 #define	FDC_SEEK_BACKWARD	!FDC_SEEK_FORWARD
 
-// These parameters are specific to the Yamaha QX1
-//	Track format=ISOIBM_MFM_ENCODING
-//	2 sides, 160 tracks/side (775072 bytes/side)
-//	The first byte on each sector is Data Address Mark (i.e. not a data byte)
-//	zone 0 (track 0 to 79):   819200 bytes (5 sectors/track * 2 sides, 1024 bytes/sector = 10240 bytes/track)
-//	zone 1 (track 80 to 159): 728064 bytes (9 sectors/track * 2 sides, 512 bytes/sector = 9216 bytes/track)
-//	zone CRC: 2444 bytes (2 bytes / sector)
-//	1550144 bytes/disk
-
-#define FDC_DISKS		99	// Virtual disks per SD card
-#define FDC_TRACKS		160	// Tracks per disk (from Canon MF-221 datasheet)
-#define FDC_SIDE		775072	// Bytes per side
-#define FDC_SIZE_TRACK_0	10240	// Bytes per track, zone 0
-#define FDC_SIZE_TRACK_1	9216	// Bytes per track, zone 1
-#define FDC_SIZE_SECTOR_0	1024	// Bytes per sector, zone 0
-#define FDC_SIZE_SECTOR_1	512	// Bytes per sector, zone 1
-#define FDC_SECTORS_0		5	// Sectors per track, zone 0
-#define FDC_SECTORS_1		9	// Sectors per track, zone 1
-
-uchar format_tracks, format_sectors;	// These are values provided by MPU
-
-// Arduino Mini pins
-#define PD0 RX
-#define PD1 TX
-#define PD2 2
-#define PD3 3
-#define PD4 4
-#define PD5 5
-#define PD6 6
-#define PD7 7
-
-#define	INT0 2
-
-#define PC0 ADC0
-#define PC1 ADC1
-#define FDC_IRQ ADC2
-#define FDC_DRQ ADC3
-
-#define OUTPUT 0xff
-#define OUTPUT 0xff
-
-// ------------------------------------------------
-// Manage U4A (74LS139) via PORTC(0,1) to drive PORTD
-//
-//  [             PORTD                     ]  PORTC
-//  -----+----+----+----+----+----+----+-----+-------------------------
-//    DB7  DB6  DB5  DB4   -   R/W   E    -  | BUS_SELECT_LCD
-//     -    -    -    -   <<    <   >    >>  | BUS_SELECT_KEYBOARD
-//     -    -    -    -   A1   A0   /WR  /RD | BUS_SELECT_ADDRESS
-//   DAL7 DAL6 DAL5 DAL4 DAL3 DAL2 DAL1 DAL0 | BUS_SELECT_DATA
-// --------------------------------------------------------------------
-
-#define BUS_SELECT_DATA	0x00
-#define BUS_SELECT_ADDRESS	0x01
-#define BUS_SELECT_KEYBOARD	0x02
-#define BUS_SELECT_LCD	0x03
 
 class	MB8877{
 	public:
@@ -212,36 +146,5 @@ Type	Command         b7 b6 b5 b4 b3 b2 b1 b0
                     i1 = 1 Ready to not ready transition
                     i0 = 1 Not ready to ready transition
 ------------------------------------------------ */
-
-// Read data
-void fdc_cmd_readdata()
-{
-  fdc.cmdtype = (fdc.cmdreg & FDC_FLAG_MULTIRECORD) ? FDC_CMD_RD_MSEC : FDC_CMD_RD_SEC;
-  if(fdc.cmdreg & FDC_FLAG_VERIFICATION) {
-    fdc.statreg = search_sector(fdc[fdc.drvreg].track, ((fdc.cmdreg & 8) ? 1 : 0), fdc.secreg, true);
-  } else {
-    fdc.statreg = search_sector(fdc[fdc.drvreg].track, fdc.sidereg, fdc.secreg, false);
-  }
-  if(!(fdc_statreg & FDC_ST_RECNFND)) fdc_statreg |= FDC_ST_BUSY;
-}
-
-void fdc_cmd_writedata()
-{
-  fdc_cmdtype = (fdc_cmdreg & 0x10) ? FDC_CMD_WR_MSEC : FDC_CMD_WR_SEC;
-  if(fdc_cmdreg & 2) {
-    fdc_statreg = search_sector(fdc[fdc_drvreg].track, ((fdc_cmdreg & 8) ? 1 : 0), fdc_secreg, true);
-  } else {
-    fdc_statreg = search_sector(fdc[fdc_drvreg].track, fdc_sidereg, fdc_secreg, false);
-  }
-  fdc_statreg &= ~FDC_ST_RECTYPE;
-  if(!(fdc_statreg & FDC_ST_RECNFND)) fdc_statreg |= FDC_ST_BUSY;
-}
-void fdc_cmd_readaddr() {; }
-void fdc_cmd_readtrack() {
-	
-}
-void fdc_cmd_writetrack() {
-	
-}
 
 #endif
